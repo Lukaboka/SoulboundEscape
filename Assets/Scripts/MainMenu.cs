c165260c382ecbd7aabf6b6bb38a139021e6177e
miurt <mariia.iurtaeva@gmail.com>
2024-01-06 00:27:47 +0100

    Story main part done, effects left

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,10 +7,7 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Texture2D cursorTexture;
     [SerializeField] private string startingScene;
-    [SerializeField] private CursorMode cursorMode = CursorMode.Auto;
-    private Vector2 hotSpot = Vector2.zero;
 
     //Animator
     [SerializeField] private Animator storyScreenFade;
@@ -19,12 +16,26 @@ public class MainMenu : MonoBehaviour
 
     [Header("Text in Story")]
     //Showing text in story
-    [SerializeField] private float wordDelay = 0.05f;
+    [SerializeField] private float wordDelay = 0.005f;
     [SerializeField] private float sentenceDelay = 0.1f;
-    [SerializeField] private float skipDelay = 0.001f;
-    [Multiline(10)]
-    [SerializeField] private string fullText;
+    [SerializeField] private float skipDelay = 0.00001f;
+    [Multiline(3)]
+    [SerializeField] private string slide01Text;
+    [SerializeField] private Sprite slide01;
+    [Multiline(3)]
+    [SerializeField] private string slide02Text;
+    [SerializeField] private Sprite slide02;
+    [Multiline(3)]
+    [SerializeField] private string slide03Text;
+    [SerializeField] private Sprite slide03;
+    [Multiline(3)]
+    [SerializeField] private string slide04Text;
+    [SerializeField] private Sprite slide04;
+    [Multiline(3)]
+    [SerializeField] private string slide05Text;
+    [SerializeField] private Sprite slide05;
     private string currentText = "";
+    [SerializeField] private int currentScreen = 1;
 
     //Story init
     private Image storyIMG;
@@ -32,15 +43,10 @@ public class MainMenu : MonoBehaviour
     private Button skipButton;
     private bool skipping = false;
 
-    //For Recording the trailer
-    public bool record = false;
-
     void Start()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-
         //Story
-        //storyIMG = GameObject.Find("StoryScreen").GetComponent<Image>();
+        storyIMG = GameObject.Find("StoryScreen").GetComponent<Image>();
         storyText = GameObject.Find("StoryText").GetComponent<TextMeshProUGUI>();
         skipButton = GameObject.Find("SkipButton").GetComponent<Button>();
         storyIMG.enabled = false;
@@ -50,9 +56,6 @@ public class MainMenu : MonoBehaviour
 
         //Transition
         transition = GameObject.Find("TransitionPaneMainMenu").GetComponent<Animator>();
-
-        if (record)
-            StartStory();
     }
 
     public void GameStart()
@@ -63,14 +66,14 @@ public class MainMenu : MonoBehaviour
 
     public void StartStory()
     {
-        //storyIMG.enabled = true;
+        storyIMG.enabled = true;
         storyText.enabled = true;
         skipButton.enabled = true;
-        //skipButton.image.enabled = true;
-        StartCoroutine(ShowText());
+        skipButton.image.enabled = true;
+        StartCoroutine(ShowText(slide01Text));
     }
 
-    IEnumerator ShowText()
+    IEnumerator ShowText(string fullText)
     {
         for (int i = 0; i < fullText.Length; i++)
         {
@@ -85,15 +88,47 @@ public class MainMenu : MonoBehaviour
                 yield return new WaitForSeconds(skipDelay);
         }
         //storyIMG.color = new Color32(255, 255, 255, 255);
-        storyScreenFade.SetTrigger("Fade");
-        yield return new WaitForSeconds(1.0f);
+
+        //storyScreenFade.SetTrigger("Fade");
+        //yield return new WaitForSeconds(1.0f);
+    }
+
+    private void changeStoryScreen()
+    {
+        currentScreen++;
+        skipping = false;
+        storyText.text = " ";
+        currentText = " ";
+        switch (currentScreen)
+        {
+            case 2:
+                storyIMG.sprite = slide02;
+                StartCoroutine(ShowText(slide02Text));
+                break;
+            case 3:
+                storyIMG.sprite = slide03;
+                StartCoroutine(ShowText(slide03Text));
+                break;
+            case 4:
+                storyIMG.sprite = slide04;
+                StartCoroutine(ShowText(slide04Text));
+                break;
+            case 5:
+                storyIMG.sprite = slide05;
+                StartCoroutine(ShowText(slide05Text));
+                break;
+            case 6: 
+            default: 
+                StartCoroutine(GoToMainGame());
+                break;
+        }
     }
 
     public void SkipButton()
     {
         if (skipping || currentText.Equals(""))
         {
-            StartCoroutine(GoToMainGame());
+            changeStoryScreen();
         }
         else
         {
